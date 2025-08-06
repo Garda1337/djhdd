3ac19397cbabf80e9ccc997141d4dd603f8593a2
Garda1337 <142615003+Garda1337@users.noreply.github.com>
2025-08-06 22:46:26 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
-header("Location: dashboard.php");
-exit;
+// Automatyczne przekierowanie po 3 sekundach
+header("Refresh:3; url=dashboard.php");
 ?>
+<html>
+  <body>
+    <p>Za chwilę nastąpi przekierowanie do panelu...</p>
+  </body>
+</html>
